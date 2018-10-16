396587221c33f515c62406eaaaa039cae1972f66
Sigrun Olafsdottir <solafsdottir@gmail.com>
2018-10-16 14:22:24 +0200

    RegistrationForm-uppgiften utökad

--- src/Övn11_Formulär/RegistrationFormB.java
+++ src/Övn11_Formulär/RegistrationFormB.java
@@ -15,17 +15,18 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos; 
 import javafx.scene.Scene; 
 import javafx.scene.control.Button; 
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane; 
 import javafx.scene.text.Text; 
 import javafx.scene.control.TextField; 
 import javafx.stage.Stage;  
 
-public class RegistrationForm extends Application { 
+public class RegistrationFormB extends Application { 
    @Override 
    public void start(Stage stage) {      
 
-      Text text1 = new Text("First name");       
-      Text text2 = new Text("Last name"); 
+      Label text1 = new Label("First name");       
+      Label text2 = new Label("Last name"); 
       Text text3 = new Text("Email"); 
        
       TextField textField1 = new TextField();   
@@ -33,7 +34,8 @@ public class RegistrationForm extends Application {
       TextField textField3 = new TextField();
       
       Button button1 = new Button("Submit"); 
-      Button button2 = new Button("Clear");  
+      Button button2 = new Button("Clear");
+      button2.setId("clearknappen");
       
       //Creating a Grid Pane 
       GridPane gridPane = new GridPane();    
@@ -52,12 +54,8 @@ public class RegistrationForm extends Application {
       gridPane.add(textField3, 1, 2); 
       gridPane.add(button1, 0, 3); 
       gridPane.add(button2, 1, 3); 
-       
-      //Styling nodes  
-      button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
-      button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
-      
-      Path outFilePath = Paths.get("src\\Övn10_Formulär\\RegisteredUsers.txt");
+
+      Path outFilePath = Paths.get("src\\Övn11_Formulär\\RegisteredUsers.txt");
       
       button1.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -86,14 +84,11 @@ public class RegistrationForm extends Application {
                 textField3.setText("");
             }
         });
-       
-      text1.setStyle("-fx-font: normal bold 20px 'serif' "); 
-      text2.setStyle("-fx-font: normal bold 20px 'serif' ");  
-      text3.setStyle("-fx-font: normal bold 20px 'serif' ");  
-      gridPane.setStyle("-fx-background-color: PURPLE;"); 
-       
+
       // Creating a scene object 
       Scene scene = new Scene(gridPane); 
+      scene.getStylesheets().add
+        (RegistrationFormB.class.getResource("Styling.css").toExternalForm());
        
       // Setting title to the Stage   
       stage.setTitle("Registrer now!"); 
